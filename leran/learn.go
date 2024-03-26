319e9016870c848293e71a97b4b235d773cf1ebb
takeshiemoto <private.takeshiemoto@gmail.com>
2024-03-27 00:04:32 +0900

    基礎

--- leran/learn.go
+++ leran/learn.go
@@ -15,5 +15,43 @@ func Run() {
 	fmt.Printf("%v+", array3)
 	fmt.Printf("%v+", slice1)
 	fmt.Printf("%v+", map1)
+}
 
+func RunPointer() {
+	x := 10
+	y := 20
+
+	px := &x
+	py := &y
+
+	// xのポインタが出力される
+	fmt.Printf("pointer px = %p\n", px)
+	// yのポインタが出力される
+	fmt.Printf("pointer py = %p\n", py)
+
+	// デリファレンスすると参照されているアドレスに保存された値が取得できる
+	// vxにはpxから取り出した値のコピーが入る
+	vx := *px
+	vy := *py
+
+	fmt.Printf("pointer px = %d\n", vx)
+	fmt.Printf("pointer py = %d\n", vy)
+
+	// ポインタを渡す
+	multiplyByTenMutable(px)
+	fmt.Printf("pxは10倍されている -> %d\n", *px)
+
+	// デリファレンスした値を渡す
+	multiplyByTenImmutable(*py)
+	fmt.Printf("pyは10倍されていない -> %d\n", *py)
+}
+
+// 元の値を10倍する関数（）
+func multiplyByTenMutable(num *int) {
+	*num = *num * 10
+}
+
+func multiplyByTenImmutable(num int) {
+	// 値に10掛けているだけ
+	num = num * 10
 }
--- main.go
+++ main.go
@@ -1,38 +1,7 @@
 package main
 
-import (
-	"encoding/csv"
-	"fmt"
-	"github.com/takeshiemoto/canaly-job/leran"
-	"io"
-	"os"
-)
+import "github.com/takeshiemoto/canaly-job/leran"
 
 func main() {
-	file, err := os.Open("data/example.csv")
-	if err != nil {
-		fmt.Println("Error opened the CSV", err)
-		return
-	}
-	defer file.Close()
-
-	reader := csv.NewReader(file)
-	if _, err := reader.Read(); err != nil {
-		fmt.Println("Error reading the header row:", err)
-	}
-	for {
-		record, err := reader.Read()
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			fmt.Println("Error reading a row:", err)
-			return
-		}
-		fmt.Println(record[0])
-		fmt.Println(record[4])
-		fmt.Println(record[5])
-	}
-
-	leran.Run()
+	leran.RunPointer()
 }
